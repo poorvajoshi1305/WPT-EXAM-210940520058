17f2beb600d48d27b0ab39512756446d7bc46e03
Poorva Joshi <poorvajoshi1305@gmail.com>
2022-01-07 19:07:32 +0530

    WPT-EXAM-210940520058

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyChatApp/>
     </div>
   );
 }
 
-export default App;
+function MyChatApp(){
+  const [inputMsg, setInputMsg]= useState("");
+  const [list,setList]=useState([]);
+  const [validate, setValid]= useState(false);
+
+  const onchangeMessage=(e)=>{
+    const newInputMsg= e.target.value;
+    setInputMsg(newInputMsg);
+  }
+
+  const sendMessage=()=>{
+    if(inputMsg==""){
+      alert("invalid message!")
+      setValid(true);
+      return;
+   }
+    const data = {
+      inputMsg : inputMsg,
+    };
+    let newList = [...list,data];
+    setList(newList);
+    setInputMsg("");
+    setValid(false);
+  }
+
+  return (
+    <div className='container mt-3 mb-2'>
+      <div className='d-flex align-text-bottom bg-dark h-50 text-light'>
+        <h1>MyChatApp <span className='fs-4'>Poorva Joshi 210940520058</span></h1>
+      </div>
+      <div className='d-flex mt-5 justify-content-around'>
+
+      <input 
+      className={
+        inputMsg=="" && validate
+        ? "form-control border border-danger" 
+        : "form-control border border-dark"
+      }
+      type="text"
+       value={inputMsg} 
+      placeholder="Lets chat here..."
+      onChange={onchangeMessage}
+      />
+      <input className='btn btn-dark mb-3' type="button" value="Send"  onClick={sendMessage}/>
+      </div>
+
+    {list.map((item,index)=>(
+     <div key={index}
+     className={
+       index % 2 == 0
+       ? 'd-flex alert alert-info d-flex justify-content-start mt-3'
+       : 'd-flex alert alert-secondary d-flex d-flex justify-content-end mt-3'
+     }
+     >
+          {item.inputMsg}
+     </div>
+    ))}
+    </div>
+  );
+}
+ 
